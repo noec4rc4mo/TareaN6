f76d9a6c927e618441e574eaa94a899fbcc124d1
noec4rc4mo <87220432+noec4rc4mo@users.noreply.github.com>
2021-07-14 22:23:32 -0600

    Add files via upload

---
+++ TareaModulo4/src/main/java/helpers/BayernM.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+/**
+ *
+ * @author noec
+ */
+public class BayernM extends Equipo{
+
+    @Override
+    public String getEquipo() {
+        return "Bayern Múnich";
+    }
+
+    @Override
+    public String getCapitan() {
+      return "Manuel Neuer - Alemania";
+    }
+    
+    
+}
---
+++ TareaModulo4/src/main/java/helpers/RealMadrid.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+/**
+ *
+ * @author noec
+ */
+public class RealMadrid extends Equipo{
+
+    @Override
+   public String getEquipo() {
+     return "Real Madrid";
+    }
+
+    @Override
+    public String getCapitan() {
+     return "Marcelo Vieira - Brasil";
+    }
+    
+    
+}
